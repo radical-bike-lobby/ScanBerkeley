751bed07e00389cb9049a0309d2b0c3e86e6973e
Naveen Gattu <naveen.gattu@gmail.com>
2023-12-07 20:22:32 -0800

    s3 upload. Upgrade trunk-recorder

--- Trunk-Recorder/transcribe.sh
+++ Trunk-Recorder/transcribe.sh
@@ -9,12 +9,6 @@ if ! [[ -f $wav ]] || ! [[ -f $json ]]; then
   exit 1
 fi
 
-if ! which jq &>/dev/null; then
-  # Try to install jq if it's not already installed
-  curl -Ls https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > /usr/local/bin/jq
-  chmod +x /usr/local/bin/jq
-fi
-
 SYSTEM="$(jq -r '.short_name' $json)"
 TALKGROUP="$(jq -r '.talkgroup' $json)"
 
@@ -23,14 +17,30 @@ if [[ "$(jq -r '.call_length' $json)" -lt "${MIN_CALL_LENGTH:-2}" ]]; then
   exit
 fi
 
-API_BASE_URL="REDACTED_CHANGE_ME"
-API_KEY="REDACTED_CHANGE_ME"
+file=$wav
+bucket=scanner-berkeley
+resource="/${bucket}/${file}"
+contentType="binary/octet-stream"
+dateValue=`date -R`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=AKIAYE233G7TVMFIBCMI
+s3Secret=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
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -L -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${file}
 
-curl -s --connect-timeout 1 --request POST \
-    --url "$API_BASE_URL/tasks" \
-    --header "Authorization: Bearer $API_KEY" \
-    --header 'Content-Type: multipart/form-data' \
-    --form call_audio=@$wav \
-    --form call_json=@$json &>/dev/null &
+# API_BASE_URL="REDACTED_CHANGE_ME"
+# API_KEY="REDACTED_CHANGE_ME"
+
+# curl -s --connect-timeout 1 --request POST \
+#     --url "$API_BASE_URL/tasks" \
+#     --header "Authorization: Bearer $API_KEY" \
+#     --header 'Content-Type: multipart/form-data' \
+#     --form call_audio=@$wav \
+#     --form call_json=@$json &>/dev/null &
 disown
 # We run the curl command as a background process and disown it to not hang up trunk-recorder.
