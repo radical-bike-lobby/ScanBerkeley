c087ef520fa216cf31e723980b0603087f294546
Naveen Gattu <naveen.gattu@gmail.com>
2024-08-12 11:38:52 -0700

    Update app.go

--- Trunk-Transcribe/app.go
+++ Trunk-Transcribe/app.go
@@ -324,7 +324,7 @@ func handleTranscription(ctx context.Context, config *Config, r *http.Request) e
 		
 		writer.WriteField("key", rdioScannerSecret)
 		writer.WriteField("meta", string(callJson))
-		writer.WriteField("system", 2) // "eastbay" system
+		writer.WriteField("system", "2") // "eastbay" system
 		part, _ := writer.CreateFormFile("audio", filename)
 		defer part.Close()
 		
